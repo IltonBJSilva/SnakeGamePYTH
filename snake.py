4e1aadeb4ba58b98a7f046a20241808a565baa81
Ilton Batista da Silva Júnior <xlsonplay@gmail.com>
2021-09-01 20:58:53 -0300

    Criar a cobra - Ok Direções - Ok Ponto inicial - Ok Cor da cobra - Ok

--- snake.py
+++ snake.py
@@ -7,11 +7,29 @@ screen = pygame.display.set_mode((600,600))
 #Titulo
 pygame.display.set_caption('Snake')
 
+#Direções
+up = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+#Lista para a cobra, e uma tupla
+snake = [(200, 200), (210, 200),(220,200)]
+snake_skin = pygame.Surface((10,10))
+#Cor da cobra
+snake_skin.fill((255,255,255))
+#Vai iniciar indo para direita
+my_direction = LEFT
+
 while True:
     #Pegar os eventos que forem feito dentro do jogo
     for event in pygame.event.get():
        if event.type == QUIT:
            pygame.quit()
 
-    pygame.display.update()
+    screen.fill((0,0,0))
+    for pos in snake:
+        #Gerar a cobra com uma tupla
+        screen.blit(pygame.Surface((10,10)),pos)
 
+    pygame.display.update()
